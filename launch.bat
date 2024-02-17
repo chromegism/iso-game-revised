71e4ed0e2dce359576a83f56133dfe2ab7ccaf15
Ruan Kellard <ruankellard@gmail.com>
2024-02-17 23:14:43 +0000

    i want to fucking kill myself

--- launch.bat
+++ launch.bat
@@ -6,16 +6,31 @@ if "%1" == "python" (
     echo:
 
     python build.py %2
+    
 ) else if "%1" == "c++" (
     echo:
     echo ----- COMPILING -----
     echo:
 
-    g++ build.cpp -Iinclude -Llib -o build
+    g++ -std=c++17 build.cpp -Iinclude -Llib -o build
 
     echo:
     echo ----- OUTPUT -----
     echo:
 
     build.exe %2
+
+) else if "%1" == "cpp" (
+    echo:
+    echo ----- COMPILING -----
+    echo:
+
+    g++ -std=c++17 build.cpp -Iinclude -Llib -o build
+
+    echo:
+    echo ----- OUTPUT -----
+    echo:
+
+    build.exe %2
+    
 )
