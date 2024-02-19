003294919dd4c29543fe103373c7c1b5ba5a0800
Ruan Kellard <ruankellard@gmail.com>
2024-02-19 16:20:40 +0000

    added chunks, but no chunk groups yet

--- build.cpp
+++ build.cpp
@@ -6,6 +6,7 @@
 #include <math.h>
 #include <vector>
 #include <filesystem>
+#include <algorithm>
 #undef main
 
 #define LOGICAL_WIDTH  1280
@@ -72,7 +73,73 @@ class Tile
 };
 
 
-SDL_Point getsize(SDL_Texture *texture) {
+class TileGroup
+{
+	public:
+		vector<Tile> tiles;
+
+		bool is_in_bounds(Tile tile, int x_off, int y_off, int viewport_size_x, int viewport_size_y)
+		{
+			if (tile.rect.x + x_off + tile.rect.w >= 0 && tile.rect.x + y_off <= viewport_size_x)
+			{
+				if (tile.rect.y + x_off + tile.rect.h >= 0 && tile.rect.y + y_off <= viewport_size_y)
+				{
+					return true;
+				}
+			}
+
+			return false;
+		}
+
+		void draw(SDL_Renderer *renderer, int viewport_pos_x, int viewport_pos_y, int viewport_size_x, int viewport_size_y)
+		{
+			for (Tile tile : tiles)
+			{
+				if (is_in_bounds(tile, viewport_pos_x, viewport_pos_y, viewport_size_x, viewport_size_y))
+				{
+					SDL_RenderCopy(tile.renderer, tile.texture, NULL, &tile.rect);
+				}
+			}
+		}
+};
+
+
+class Chunk
+{
+	public:
+		int x;
+		int y;
+
+		SDL_Renderer *renderer;
+
+		TileGroup tile_group;
+
+		void draw(int viewport_pos_x, int viewport_pos_y, int viewport_size_x, int viewport_size_y)
+		{
+			tile_group.draw(renderer, viewport_pos_x, viewport_pos_y, viewport_size_x, viewport_size_y);
+		}
+
+		void sort_tiles()
+		{
+			
+		}
+
+		void add_tile(Tile tile)
+		{
+			tile_group.tiles.push_back(tile);
+		}
+
+		void add_tiles(vector<Tile> tiles)
+		{
+			for (Tile tile : tiles)
+			{
+				add_tile(tile);
+			}
+		}
+};
+
+
+SDL_Point gettexturesize(SDL_Texture *texture) {
     SDL_Point size;
     SDL_QueryTexture(texture, NULL, NULL, &size.x, &size.y);
     return size;
@@ -84,7 +151,7 @@ Tile load_tile(SDL_Renderer *renderer, const char *file)
 	Tile t;
 	t.renderer = renderer;
 	t.texture = IMG_LoadTexture(renderer, file);
-	t.size = getsize(t.texture);
+	t.size = gettexturesize(t.texture);
 	t.rect.x = 0;
 	t.rect.y = 0;
 	t.rect.w = t.size.x;
@@ -139,14 +206,23 @@ int main(int argc, char* argv[])
 	int texture_count = 0;
 	for (const auto & entry : fs::directory_iterator("layers")) {
 		fs::path p = entry.path();
-    	layer_textures.push_back(load_tile(renderer, p.string().c_str()));
+		Tile tile = load_tile(renderer, p.string().c_str());
+		tile.rect.x = gen_rand(0, LOGICAL_WIDTH - 64);
+		tile.rect.y = gen_rand(0, LOGICAL_HEIGHT - 48);
+    	layer_textures.push_back(tile);
 		texture_count++;
   	}
+
+	int viewport_pos_x = 0;
+	int viewport_pos_y = 0;
 
     bool running = true;
 	SDL_Event event;
 
 	SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+
+	Chunk ch;
+	ch.add_tiles(layer_textures);
 
     while (running)
     {
@@ -171,7 +247,9 @@ int main(int argc, char* argv[])
 		}
 
 		// SDL_RenderCopy(renderer, layer_textures[0].texture, NULL, &layer_textures[0].rect);
-		layer_textures[gen_rand(0, 8)].render();
+		// layer_textures[gen_rand(0, 8)].render();
+
+		ch.draw(viewport_pos_x, viewport_pos_y, LOGICAL_WIDTH, LOGICAL_HEIGHT);
 
 		SDL_RenderPresent(renderer);
     }
