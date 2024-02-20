4e6ed1203c0ff941939fafab077a906d0df48a17
Ruan Kellard <ruankellard@gmail.com>
2024-02-20 22:10:30 +0000

    oopsies

--- build.sh
+++ build.sh
@@ -6,7 +6,23 @@ then
 
     python3 main.py $2
 fi
+
 if [ "$1" == "c++" ]
+then
+    echo
+    echo "----- COMPILING -----"
+    echo
+
+    g++ main.cpp -Wall -lSDL2main -lSDL2 -lSDL2_image -o main_deb
+
+    echo
+    echo "----- OUTPUT -----"
+    echo
+
+    ./main_deb
+fi
+
+if [ "$1" == "cpp" ]
 then
     echo
     echo "----- COMPILING -----"
