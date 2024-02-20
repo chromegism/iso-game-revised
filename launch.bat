422c33e0246eda7344eb2a4d7f83ed6621b362af
Ruan Kellard <ruankellard@gmail.com>
2024-02-20 22:07:47 +0000

    adapted to allow for ubuntu / debian compilation

--- launch.bat
+++ launch.bat
@@ -12,25 +12,25 @@ if "%1" == "python" (
     echo ----- COMPILING -----
     echo:
 
-    g++ -std=c++17 build.cpp -I"C:\Cincludes64" -L"C:\Clibs64" -Wall -lSDL2main -lSDL2 -lSDL2_image -o build
+    g++ -std=c++17 main.cpp -I"C:\Cincludes64" -L"C:\Clibs64" -Wall -lSDL2main -lSDL2 -lSDL2_image -o main
 
     echo:
     echo ----- OUTPUT -----
     echo:
 
-    build.exe %2
+    main.exe %2
 
 ) else if "%1" == "cpp" (
     echo:
     echo ----- COMPILING -----
     echo:
 
-    g++ -std=c++17 build.cpp -I"C:\Cincludes32" -L"C:\Clibs32" -Wall -lmingw32 -lSDL2main -lSDL2 -o build
+    g++ -std=c++17 main.cpp -I"C:\Cincludes32" -L"C:\Clibs32" -Wall -lmingw32 -lSDL2main -lSDL2 -o main
 
     echo:
     echo ----- OUTPUT -----
     echo:
 
-    build.exe %2
+    main.exe %2
 
 )
